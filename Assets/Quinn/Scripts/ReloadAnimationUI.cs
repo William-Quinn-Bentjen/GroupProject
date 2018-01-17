a0c8dd3a48c2bd33112767ffa5e9d5e036340646
William Bentjen <s179051@students.aie.edu.au>
2018-01-17 11:15:22 -0800

    UI work

--- Assets/Quinn/Scripts/ReloadAnimationUI.cs
+++ Assets/Quinn/Scripts/ReloadAnimationUI.cs
@@ -5,25 +5,43 @@ using UnityEngine.UI;
 
 public class ReloadAnimationUI : MonoBehaviour {
     //getting tired (sorry for names)
-    public Slider Slidelerp;
-    public float Duration;
+    public Slider ReloadBar;
+    public float Duration = 1;
+    //private
     private float currentTime;
     private bool lerping = false;
 	// Use this for initialization
 	void Start () {
-		
+        StartLerp();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (lerping)
         {
-            currentTime -= Time.deltaTime;
-            if (currentTime <= 0)
+            currentTime += Time.deltaTime;
+            if (currentTime >= Duration)
             {
                 lerping = false;
+                ReloadBar.value = ReloadBar.maxValue;
+            }
+            else
+            {
+                ReloadBar.value = Mathf.Lerp(ReloadBar.minValue, ReloadBar.maxValue, currentTime / Duration);
             }
         }
 	}
-    public void on
+    //start reload animation with specified duratuion set to 0/leave to use the duration that we currently have
+    public void StartLerp(float duration = 0)
+    {
+        if (duration > 0)
+        {
+            Duration = duration;
+        }
+        currentTime = 0;
+        ReloadBar.value = ReloadBar.minValue;
+        lerping = true;
+    }
+    public void 
+    
 }
