cee1ff223f138f1215d9d34c39054e295346f746
William Quinn Bentjen <s179051@students.aie.edu.au>
2018-01-17 06:52:47 -0800

    inital commit

---
+++ Assets/Quinn/Scripts/ReloadAnimationUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReloadAnimationUI : MonoBehaviour {
+    //getting tired (sorry for names)
+    public Slider Slidelerp;
+    public float Duration;
+    private float currentTime;
+    private bool lerping = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lerping)
+        {
+            currentTime -= Time.deltaTime;
+            if (currentTime <= 0)
+            {
+                lerping = false;
+            }
+        }
+	}
+    public void on
+}
---
+++ Assets/Quinn/Scripts/SpeedControlUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedControlUI : MonoBehaviour {
+    //public
+    public Slider Throttle;
+    public Text Speedometer;
+    public string SpeedometerPrefix = "Speed: ";
+    public string SpeedometerSuffix = " m/s";
+    //private
+    private float SpeedometerValue;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Sets the speedometer text to that speed
+    public void SpeedometerSet()
+    {
+        Speedometer.text = SpeedometerPrefix + Throttle.value + SpeedometerSuffix;
+    }
+}
