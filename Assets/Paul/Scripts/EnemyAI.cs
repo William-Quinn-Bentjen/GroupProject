93ed3a6c9085c5cde98635c01e4c031b1d96bc05
Paul Gibbons <s179054@students.aie.edu.au>
2018-01-19 21:30:23 -0800

    AI work
    Added Missile Ai and Camera Script. Enemy Ai is WIP.

---
+++ Assets/Paul/Scripts/EnemyAI.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    [Header("Speed and sensitivity")]
+    public float speed;
+    public float homingSensitivity;// 0 means less tracking, 1 is most tracking possible
+
+    [Header("RigidBody Target")]
+    public Rigidbody target;
+
+    [Header("Non-editable values")]
+    public float metersPerSec;
+
+
+    Rigidbody myRig;
+
+    Collider myCol;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRig = GetComponent<Rigidbody>();
+        myCol = GetComponent<Collider>();
+    }
+
+    public enum CurrentAction
+    {
+        Attack = 0, Advance = 1, Retreat = 2
+    }
+
+    public enum AttackType
+    {
+        Missile = 0, Railgun = 1, MachineGun = 2
+    }
+
+    public CurrentAction actionType = new CurrentAction();
+    public AttackType attackActionType = new AttackType();
+    // Update is called once per frame
+    void Update()
+    {
+        //Measurments-----------------------------------------------------------------------------------------------------------------------------------------------------
+        metersPerSec = myRig.velocity.magnitude;
+
+        
+        //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        switch (actionType)
+        {
+
+        }
+
+        Vector3 relativePos = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, homingSensitivity);//the main bit of tracking.
+
+        myRig.AddRelativeForce(new Vector3(0, 0, speed) * Time.deltaTime);//This uses rigidbody and looks more real. Best setting for drag and mass is 0.5(drag) to 1(mass)
+        //transform.Translate(0, 0, speed * Time.deltaTime, Space.Self);//This option does not use rigidbody but is far more accurate
+
+
+
+
+    }
+
+    void onCollisionEnter(Collider col)
+    {
+
+        if (col.gameObject.name == "Target")
+        {
+            Debug.Log("detected Collision");
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Paul/Scripts/PlayerCam.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    //This is the first person cam controls
+    public float speed;
+
+    public float mouseSensX = 3.5f;//Sensativity x
+    public float mouseSensY = 3.5f;//sensativity Y
+
+    Transform camT;
+    Rigidbody myRig;
+    float vertLookRotation;
+
+    // Use this for initialization
+    void Start()
+    {
+        camT = Camera.main.transform;
+        myRig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        myRig.AddRelativeTorque(Vector3.up * Input.GetAxis("Mouse X") * mouseSensX);//do not multiply by time.deltatime as mouse input is frame independant
+
+        myRig.AddRelativeTorque(Vector3.left * Input.GetAxis("Mouse Y") * mouseSensY);
+
+        //vertLookRotation = Mathf.Clamp(vertLookRotation, -60, 60); I guess since this is a full 3D combat game, clamping the players view range is kinda pointless.
+
+        //Movement controls
+        if (Input.GetKey(KeyCode.W))
+        {
+            myRig.AddRelativeForce(new Vector3(0, 0, speed) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            myRig.AddRelativeForce(new Vector3(0, 0, -speed) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            myRig.AddRelativeForce(new Vector3(speed, 0, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            myRig.AddRelativeForce(new Vector3(-speed, 0, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            myRig.AddRelativeForce(new Vector3(0, speed, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.C))
+        {
+            myRig.AddRelativeForce(new Vector3(0, -speed, 0) * Time.deltaTime);
+        }
+
+
+        //cam tilt controls
+        if (Input.GetKey(KeyCode.E))
+        {
+            myRig.AddRelativeTorque(Vector3.forward * ((-speed / 3) * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            myRig.AddRelativeTorque(Vector3.forward * ((speed / 3) * Time.deltaTime));
+        }
+    }
+}
