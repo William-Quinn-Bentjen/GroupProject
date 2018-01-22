b63e3791f4eeaf45354715102f452ec0ab9a3b8d
Unknown <Druffel12@gmail.com>
2018-01-22 02:02:02 -0800

    Movement Work

---
+++ Assets/Cole/PlayerControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float speed;
+    public float SetSpeed;
+
+    public float grow = 0;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void pitchIncrement(float pitch)
+    {
+        
+    }
+
+    void rollIncrement(float roll)
+    {
+        gameObject.gameObject.rotation.y += float amount;
+    }
+
+    void yawIncrement(float yaw)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(horizontal, 0, vertical);
+        rb.AddForce(move * speed * Time.deltaTime);
+
+    }
+}
